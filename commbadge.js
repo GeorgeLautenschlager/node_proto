7632eb480bd1a74a6088125142afe40b24a14dea
George Lautenschlager <georgelautenschlager@gmail.com>
2019-04-02 13:04:39 -0400

    play an episode

--- commbadge.js
+++ commbadge.js
@@ -1,12 +1,13 @@
 const http = require('http');
+const spawn = require('child_process');
 
 const hostname = '127.0.0.1';
-const port = 3000;
+const port = 4000;
 
 const server = http.createServer((req, res) => {
-  const command = ""
+  const command = "vlc \"/run/user/1000/gvfs/afp-volume:host=LCARS.local,user=lautenschlager,volume=video/Shows/It's always Sunny in Philadelphia/Season 3/15 The Gang Dances Their Asses Off.avi\""
 
-  let stdout = execSync(command);
+  let stdout = spawn.execSync(command);
 
   res.statusCode = 200;
   res.setHeader('Content-Type', 'application/json');
